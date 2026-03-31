098e5b72f192f463493049ab567470c4ff8723f4
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-03-31 21:37:19 +0530

    js practice

--- Day 18/return_keyword.js
+++ Day 18/return_keyword.js
@@ -1,7 +1,7 @@
 // return keyword is used to return some value from the function.
 
-function isAdult(age){
-    if(age >= 18){
+function isAdult(age) {
+    if (age >= 18) {
         return "adult";
     } else {
         return "not adult";
@@ -19,12 +19,24 @@ console.log(isAdult(13));
 // practice Qs 
 // create a function that return the sum of numbers from 1 to n
 
-function getSum(n){
+function getSum(n) {
     let sum = 0;
-    for(let i = 1; i <= n; i++ ){
+    for (let i = 1; i <= n; i++) {
         sum = sum + i;
     }
     return sum;
 }
 
 getSum(100);
+
+
+// practice qs
+// create a function that return that concatenation of all strings in an array
+let str = ["hello", "namaste", "radhe-radhe", "ram-ram", "!"];
+function concat(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        result = result + str[i]; // result += str[i];
+    }
+     return result;
+}
