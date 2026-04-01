4ab39c4a75489e8eb2ebd6094a0ef57af86f6bfd
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-01 22:15:21 +0530

    some practice

---
+++ Day 18/method.js
@@ -0,0 +1,13 @@
+// action that can be performed on an object
+const calculate = {
+    add: function (a, b) {
+        return a + b;
+    },
+    sub:  function (a, b){
+        return a - b;
+    },
+    mul: function (a, b){
+        return a * b;
+    }
+};
+calculate.add(1,2);
