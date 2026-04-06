0dd908c3b19d11b533378acf61b28b3c547bd8b3
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-06 20:19:46 +0530

    practie

---
+++ Day 19/try_and_catch.js
@@ -0,0 +1,19 @@
+// console.log("hello");
+// console.log("hello");
+// console.log(a);
+// console.log("hello2");
+// console.log("hello2");
+// console.log("hello2");
+
+console.log("hello");
+console.log("hello");
+// let a = 5;
+try{
+console.log(a);
+}catch{
+    console.log("error");
+    
+}
+console.log("hello2");
+console.log("hello2");
+console.log("hello2");
