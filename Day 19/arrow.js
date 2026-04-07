07e3c1b8d470af4629f085acf5f9d0560c299469
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-07 20:54:09 +0530

    NEW

---
+++ Day 19/arrow.js
@@ -0,0 +1,20 @@
+// arrow function
+
+const sum = (a, b) => {
+    console.log(a + b);
+};
+
+// sum(5, 6);
+
+const cube = (n) => {
+    return n * n * n;
+};
+
+// cube(40);
+
+
+const pow = (a, b) => {
+    return a ** b;
+};
+
+// pow(2, 2);
