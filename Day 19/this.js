32d2d8dc70284dc96148f170cae5024b47cb8814
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-08 20:14:12 +0530

    practice

--- Day 19/this.js
+++ Day 19/this.js
@@ -18,3 +18,26 @@ show();
 
 // arrow function me this 
 
+const user1 = {
+    name: "tanishq",
+    greet: () => {
+        console.log(this.name);
+    }
+};
+user1.greet(); // output --> undefined
+
+
+
+
+
+const obj = {
+    name: "tanishq",
+    show: function () {
+        const inner = () => {
+            console.log(this.name)
+        };
+        inner();
+    }
+};
+
+console.log(obj.show());
