3945197c5716413d3a9ee8d2c31025fcb29727c9
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-09 18:34:17 +0530

    practice

--- Day 19/this.js
+++ Day 19/this.js
@@ -1,3 +1,20 @@
+
+const student ={
+    name : "tanishq",
+    age: 23,
+    eng: 95,
+    math: 93,
+    phy: 97,
+    getAvg(){
+        console.log(this);
+        let avg = (this.eng + this.math + this.phy)/3;
+         console.log(`${this.name}, got avg marks = ${avg}` );
+    }
+}
+
+student.getAvg();
+
+
 //  object ke andar
 const user = {
     name: "tanishq",
