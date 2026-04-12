4b0517f6a6c9ee83d607b777dffe4d83615a2752
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-12 19:08:18 +0530

    practice

--- Day 20/forEach.js
+++ Day 20/forEach.js
@@ -1,8 +1,24 @@
 // arr.forEach(some function definition and name )
 
-let arr = [1,2,3,4,5];
-function print(el){
+let arr = [1, 2, 3, 4, 5];
+function print(el) {
     console.log(el);
 }
 
 arr.forEach(print);
+
+
+
+//or 
+
+arr.forEach(function (el) {
+    console.log(el);
+});
+
+
+
+
+let friends = ["Rahul", "Aman", "Priya"];
+friends.forEach(function (el) {
+    console.log(el)
+});
