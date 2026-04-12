c6d86abffa5f1d94273a70564f730172fde1234e
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-12 20:21:30 +0530

    practice

--- Day 20/forEach.js
+++ Day 20/forEach.js
@@ -17,8 +17,35 @@ arr.forEach(function (el) {
 
 
 
+// arrow function
+// let students = ["rohit", " viveek", " puneet"];
 
 let friends = ["Rahul", "Aman", "Priya"];
-friends.forEach(function (el) {
-    console.log(el)
-});
+const  indexName = (name, i) => {
+        console.log(`${i}, - ${name}`);
+};
+
+friends.forEach(indexName);   
+
+
+// Maan lo tumhare paas ek numbers ka array hai:
+// let numbers = [2, 4, 6];
+
+// Mujhe ek arrow function ke sath forEach loop likh kar dikhao jo is array ke har number ka square print kare (yani number ko usi number se multiply kare).
+// Output sirf yeh aana chahiye:
+// 4
+// 16
+// 36
+
+let numbers = [2, 4, 6];
+const square = (el) => {
+   console.log(el*el);
+}
+
+numbers.forEach(square);
+
+
+
+
+
+
