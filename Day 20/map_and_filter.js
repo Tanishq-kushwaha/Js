37e8d2e6813e95fe767c5a3fd0420df499cb8680
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-14 21:57:15 +0530

    how to update any array

--- Day 20/map_and_filter.js
+++ Day 20/map_and_filter.js
@@ -10,7 +10,7 @@ let double = num.map(function (el) {
 console.log(double);
 
 
-
+// square of numbers in an array by using map function
 
 let numbers = [2, 4, 6];
 
@@ -19,3 +19,17 @@ let result = numbers.map((el) => {
 });
 
 console.log(result);
+
+// Maan lo tum meri tech company ke lead developer ho. Humare paas humare top employees  ki current salaries  ka ek data hai:
+
+let salaries = [50000, 80000, 120000];
+
+// Company ne acha profit banaya hai aur maine as a strategist decide kiya hai ki hum in sabko 10% bonus denge.
+
+// Tumhe map method ka use karke ek naya array  generate  karna hai, jisme har employee ki salary mein 10% ka increment  add ho gaya ho.
+
+
+let updatedSalaries = salaries.map((salary) => {
+    return (salary * 1.1);
+});
+console.log(updatedSalaries);
