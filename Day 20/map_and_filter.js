1bff5b13587a075d8917f3c468a2d0118b714da3
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-14 21:15:01 +0530

    map_and_filter

--- Day 20/map_and_filter.js
+++ Day 20/map_and_filter.js
@@ -1,3 +1,10 @@
 // map_and_filter
 
 // let newArr = Array.map(some function deffinition or name )
+
+let num = [1, 2, 3, 4];
+let double = num.map(function (el) {
+    return el * 2;
+});
+
+console.log(double);
