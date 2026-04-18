e47250f144e8dd22ccfc5e03d3e25ae00a6050c6
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-18 21:19:34 +0530

    ["hello", "world", "js"] — map use karke sabhi strings ko uppercase me convert kiya

--- Day 20/revison.js
+++ Day 20/revison.js
@@ -7,4 +7,26 @@ let arr = [10, 20, 30];
 let square = arr.forEach((el) => {
     console.log(el * el);
 });
+
 console.log(square);
+
+
+
+// Q2 — map
+// ["hello", "world", "js"] — map use karke sabhi strings ko uppercase me convert karo.
+
+
+let array = ["hello", "world", "js"];
+
+let upper = array.map((str) => {
+     return str.toUpperCase();
+});
+
+console.log(upper);
+
+
+// Q3 — filter
+// [5, 12, 3, 18, 7, 25] — sirf 10 se bade numbers ka array banao.
+
+
+let numbers = [5, 12, 3, 18, 7, 25];
