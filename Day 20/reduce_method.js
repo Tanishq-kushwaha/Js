d2b028999d511e329e5d3547d751c972ecf188ea
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-20 21:31:59 +0530

    company ki sales

--- Day 20/reduce_method.js
+++ Day 20/reduce_method.js
@@ -2,6 +2,27 @@
 
 // arr.reduce(reducer function with 2 variables for (accumulator, element);
 
-let sum  = [1,2,3,4].reduce((res, el)=> (res+el))
+let sum = [1, 2, 3, 4].reduce((res, el) => (res + el));
 
-console.log(sum); // 
+console.log(sum); // sum of all element 
+
+
+
+
+// Humari aaj ki aakhri 4 sales ka data yeh hai:
+
+let dailySales = [1000, 2500, 3000, 1500];
+
+// Mujhe reduce method ka use karke ek code likh kar dikhao jo in saari sales ko jod de.
+// Is result ko ek variable totalRevenue mein store karo aur end mein usey console.log karo. Jiska output exact 8000 aana chahiye.
+
+
+// let sales = dailySales.reduce((total, sale) => {
+//     return total + sale;
+// }, 0);
+
+// console.log(sales);
+
+let sales = dailySales.reduce((total, sale) => (total + sale));
+
+console.log(sales);
