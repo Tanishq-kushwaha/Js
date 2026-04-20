d8e842a18c77b21c40ae492288846de034c0d7cf
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-20 21:15:08 +0530

    use of reduce method to sum of all element in array

---
+++ Day 20/reduce_method.js
@@ -0,0 +1,7 @@
+//reduce the array to a single value
+
+// arr.reduce(reducer function with 2 variables for (accumulator, element);
+
+let sum  = [1,2,3,4].reduce((res, el)=> (res+el))
+
+console.log(sum); // 
