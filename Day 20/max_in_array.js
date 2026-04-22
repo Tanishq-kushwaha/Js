9490c6a1f35073ff3ace2a7479c9d5d2e29e1d0a
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-22 20:05:59 +0530

    find a max no. in any array use of reduce method

--- Day 20/max_in_array.js
+++ Day 20/max_in_array.js
@@ -1,12 +1,26 @@
 // find a max no. in any array
 
 let arr = [1, 2, 3, 4, 4, 5, 6, 7];
-let max = 0;
-for(let i = 0; i < arr.length; i++){
-    if(max < arr[i]){
-     max = arr[i];
+// let max = 0;
+// for(let i = 0; i < arr.length; i++){
+//     if(max < arr[i]){
+//      max = arr[i];
+//     }
+// };
+
+// console.log(max);
+
+
+
+// find a max no. in any array use of reduce method 
+
+let max = arr.reduce((max, el)=>{
+    if(max < el){
+        return el;
+    }else {
+        return max;
     }
-};
+});
 
 console.log(max);
 
