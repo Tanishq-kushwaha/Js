22d39fbf2da4a8990162e8efe9b6ab4234ce944a
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-22 20:22:01 +0530

    check if all number in our array are multiples of 10 or not

---
+++ Day 20/practice_6_qs.js
@@ -0,0 +1,13 @@
+//  check if all number in our array are multiples of 10 or not
+
+let arr = [1, 2, 30, 40, 50,];
+
+let ans = arr.every((el) => {
+    if (el % 10 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+});
+
+console.log(ans)
