d5944559a70b0c930bd2d7770a0758e4a1ee9aa3
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-24 21:41:59 +0530

    Task

--- Day 20/spread.js
+++ Day 20/spread.js
@@ -1,1 +1,28 @@
-// 
+// Expends an iterable into multiple value 
+
+// use or three dot ->(...)
+
+// function func(...arr){
+//     do something
+// }
+
+console.log(..."TanishqKushwaha");
+
+
+let arr = ["karan", "shradaha", "vivek"];
+
+console.log(...arr);
+
+
+
+
+// Task:
+// Tumhe Spread operator (...) ka use karke ek naya array banana hai jiska naam ho allStars. Us ek array ke andar dono teams ke naam aane chahiye, aur end mein usey console.log karke dikhana hai.
+
+
+let techTeam = ["Rahul", "Aman"];
+let salesTeam = ["Priya", "Neha"];
+
+let allStars = [...techTeam, ...salesTeam];
+
+console.log(...allStars);
