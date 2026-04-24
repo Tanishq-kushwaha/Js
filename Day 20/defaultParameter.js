0f1017bd90267893ee04a3723eb597fe46e15833
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-04-24 21:17:58 +0530

    use of default parameter

--- Day 20/defaultParameter.js
+++ Day 20/defaultParameter.js
@@ -1,1 +1,13 @@
-// 
+// giving a default value to the argument
+
+// function func(a, b = 2){
+//    do something
+// }
+
+function sum(a, b = 3) {
+    return a + b;
+}
+
+console.log("a ki value apney di lekin  b ki default value 3 hai is liye jab b ki value nahi dogey to wo automatically 3 hi lega  is liye sum(2) =", sum(2));
+
+console.log("lekin jab a aur b dono ki value aab hi decide kare jese sum(2, 4) =",sum(2, 4));
