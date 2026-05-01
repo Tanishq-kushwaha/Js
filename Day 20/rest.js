000c9cb4bab11fc036722b8eae5c5b7fc32d3b03
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-01 21:32:07 +0530

    use of destructuring and rest operator

--- Day 20/rest.js
+++ Day 20/rest.js
@@ -3,3 +3,26 @@
 function sum(...args){
     return args.reduce((add,el)=> add=el);
 }
+
+
+//problem
+
+
+let topPerformers = ["Rahul", "Aman", "Priya", "Neha", "Karan"];
+
+// Task:
+// Tumhe Destructuring aur Rest operator ka use karke ek hi line mein yeh kaam karna hai:
+
+// Sabse pehle naam ko ek variable mein dalo jiska naam ho bestEmployee.
+
+// Baaki ke saare bache hue naamo ko Rest operator ka use karke ek naye array  mein samet lo, jiska naam ho regularEmployees.
+
+// End mein dono ko console.log karke dikhao. (Jaise: console.log(bestEmployee, regularEmployees);)
+
+
+
+let [bestEmployee, ...regularEmployee] = topPerformers;
+
+console.log(bestEmployee);//Rahul
+
+console.log(regularEmployee);//(4) ['Aman', 'Priya', 'Neha', 'Karan']
