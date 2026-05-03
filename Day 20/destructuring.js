f3b4adccd92ccd565d995bdb31edd8909a5d8ae2
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-03 20:34:44 +0530

    refactor: update destructuring assignment for clarity and maintainability

--- Day 20/destructuring.js
+++ Day 20/destructuring.js
@@ -1,5 +1,9 @@
 
 
 let names = ["tony","bruce","steve","peter"];
-let [winner, runnerup] = names;
+
+// let winner = names[0];
+// let runnerup = names[1];
+
+let [winner, ...runnerup] = names;
 console.log(winner, runnerup);
