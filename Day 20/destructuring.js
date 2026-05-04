2eea7980ccd42c84e0bdeb12a6f0d4ef579416b3
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-04 20:00:22 +0530

    refactor: improve formatting and add nested destructuring example for laptop object

--- Day 20/destructuring.js
+++ Day 20/destructuring.js
@@ -1,9 +1,26 @@
 
 
-let names = ["tony","bruce","steve","peter"];
+let names = ["tony", "bruce", "steve", "peter"];
 
 // let winner = names[0];
 // let runnerup = names[1];
 
 let [winner, ...runnerup] = names;
 console.log(winner, runnerup);
+
+
+
+// Suppose this data is coming from your system:
+let laptop = {
+    brand: "ThinkPad",
+    specs: {
+        ram: "16GB",
+        processor: "i7"
+    }
+};
+
+// Using this new nested destructuring syntax, can you show me how to exit a processor in one line?
+
+
+let { specs: { processor } } = laptop;
+console.log(processor);
