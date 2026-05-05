d599d46524db3056dee356c899461e21ff8442f3
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-05 19:46:04 +0530

    feat: implement array squaring and logging in assignment_Qs.js

--- Day 20/assignment_Qs.js
+++ Day 20/assignment_Qs.js
@@ -1,2 +1,7 @@
 // Qs1. Square and sum the array elements using the arrow function and then find the
 // average of the array. 
+let arr = [1, 2, 3, 4, 5];
+
+let square = arr.map((el) => el * el);
+console.log(square);
+
