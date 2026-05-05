d02a0a04b08f81bc984c4f46e22019be16459d01
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-05 20:10:10 +0530

    fix: add chaining of map and reduce for sum calculation and average in assignment_Qs.js

--- Day 20/assignment_Qs.js
+++ Day 20/assignment_Qs.js
@@ -11,6 +11,13 @@ console.log(arrSum);//15
 let average = arrSum / arr.length;
 console.log(average);//3
 
+// Step 1 & 2: Chaining map and reduce
+let sum = arr.map((el) => el * el).reduce((acc, cu) => acc + cu, 0); 
+
+// Step 3: Finding Average
+let avg = sum / arr.length; 
+
+console.log(avg); // Output: 11
 
 // Qs2. Create a new array using the map function whose each element is equal to the
 // original element plus 5.
