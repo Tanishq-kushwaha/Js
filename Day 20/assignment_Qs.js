dbd66dc5be434a091d84822d6b737aa84cb2dd9a
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-05 20:21:53 +0530

    fix: better readability in assignment_Qs.js

--- Day 20/assignment_Qs.js
+++ Day 20/assignment_Qs.js
@@ -19,5 +19,9 @@ let avg = sum / arr.length;
 
 console.log(avg); // Output: 11
 
+
+
+
+
 // Qs2. Create a new array using the map function whose each element is equal to the
 // original element plus 5.
