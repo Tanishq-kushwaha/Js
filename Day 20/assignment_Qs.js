defa1e8b5814f3c5b0ce8122ad7092f5b096ecd1
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-05 19:56:54 +0530

    fix: correct variable names for clarity in array summation in assignment_Qs.js

--- Day 20/assignment_Qs.js
+++ Day 20/assignment_Qs.js
@@ -5,7 +5,7 @@ let arr = [1, 2, 3, 4, 5];
 let square = arr.map((el) => el * el);
 console.log(square);//[1, 4, 9, 16, 25]
 
-let arrSum = arr.reduce((el, next) => el + next, 0);
+let arrSum = arr.reduce((acc, current) => acc + current, 0);
 console.log(arrSum);//15
 
 let average = arrSum / arr.length;
