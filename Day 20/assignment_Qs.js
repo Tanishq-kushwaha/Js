b31908d73dceae373593a9b0b3361b906a2a4181
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-05 19:50:36 +0530

    fix: update console log statements for clarity in assignment_Qs.js

--- Day 20/assignment_Qs.js
+++ Day 20/assignment_Qs.js
@@ -3,7 +3,10 @@
 let arr = [1, 2, 3, 4, 5];
 
 let square = arr.map((el) => el * el);
-console.log(square);
+console.log(square);//[1, 4, 9, 16, 25]
 
 let arrSum = arr.reduce((el, next)=> el+next,0);
-console.log(arrSum);
+console.log(arrSum);//15
+
+let average = arrSum / arr.length;
+console.log(average);
