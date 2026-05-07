a5b8d6f80009b5ffff0de43d959dd71f756dfaf9
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-07 18:51:44 +0530

    fix: update comments for clarity in every and some method examples

--- Day 20/every_and_some.js
+++ Day 20/every_and_some.js
@@ -1,5 +1,5 @@
 // arr.every(some function definition or name);
-// AND operator ki tarah kaam karta hai agar condition ke hissab se ik bhi value false hui to output bhi false hi ayega.
+//It functions like the AND operator; if even a single value in the condition evaluates to False, the output will also be False.
 
 let data = [1, 2, 3, 4].every((el) => (el % 2 == 0));// false
 
@@ -7,10 +7,9 @@ let data2 = [6, 2, 8, 4].every((el) => (el % 2 == 0));// true
 
 console.log(data);
 console.log(data2);
+// Let's assume our tech company has developed a new VIP Club software. The security rule is that entry is granted only if all entrants are adults—that is, they are aged 18 or older.
 
-// Maan lo humari tech company ne ek naya VIP Club software banaya hai. Security rule yeh hai ki isme entry tabhi milegi jab aane wale saare log adult hon, yaani unki age 18 ya usse zyada ho.
-
-// Humare paas aaye hue users ki ages ka ek data hai:
+// We have a dataset containing the ages of the users who have arrived:
 
 
 // let userAges = [21, 25, 19, 17, 30];
