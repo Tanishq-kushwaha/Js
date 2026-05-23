ca7361544394ee7fe40b89e725c94ee25b11c6d2
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-23 20:51:38 +0530

    feat: refactor variable names and update button click functionality for background color change

--- Day 21/starter code/app.js
+++ Day 21/starter code/app.js
@@ -1,5 +1,7 @@
 // Create an empty <div> and a <button>. On click of the button, change the background color of the <div> and insert some text into it using getElementById in JavaScript.
-let click = document.getElementById("specialDiv");
-click.style.backgroundColor = "green";
+let myDiv = document.getElementById("specialDiv");
+let actionButton = document.getElementById("specialButton");
 
-click.innerText = "hello spider man";
+myDiv.innerText = "hello spider man";
+
+actionButton.style.backgroundColor = "aqua";
