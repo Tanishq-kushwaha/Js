096b049ba7cbdce3790e068d10412c6daa65f8d9
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-25 21:26:53 +0530

    feat: rename variable for the first mainImg element to spyderManImg in app.js

--- Day 21/starter code/app.js
+++ Day 21/starter code/app.js
@@ -126,5 +126,7 @@ console.log(`This is innerHtml----> ${paragraph.innerHTML}`);
 
 // Select the first <a class="boxLink"> in your HTML and use setAttribute to set its src to "https://www.google.com".
 
-let img = document.querySelector("mainImg");
+let spyderManImg = document.querySelector("mainImg");
+
+
 
