a5adedee41059d21ac5f327029a02c8901b7b0a5
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-06 19:53:31 +0530

    feat(DOM): Add comment for task to create button for changing background color

--- Day 22/app.js
+++ Day 22/app.js
@@ -7,3 +7,6 @@ document.querySelector("body").append(btn);
 btn.onclick = function(){
     document.querySelector("h1").innerText =  "Button was clicked";
 }
+// Task 2: Create a button that changes the background color of the webpage when clicked.
+
+let btn2 = document.createElement("button");
