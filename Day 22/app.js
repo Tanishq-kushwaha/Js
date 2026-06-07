20c8fabcf3367c51e18788bdc174e687a1cd1708
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-07 18:02:41 +0530

    feat(DOM): Implement character count update on input field keyup

--- Day 22/app.js
+++ Day 22/app.js
@@ -58,5 +58,10 @@ box.onmouseleave = function(){
 // 3. Using the onkeyup event, the total character length within the paragraph should be updated as soon as the user types something in the input field and releases the key.
 
 let paragraph = document.querySelector("#para1");
+let input = document.querySelector("input");
 
 
+paragraph.onkeyup = function(){
+   let charCount = input.value.length;
+    paragraph.innerText =  charCount;
+}
