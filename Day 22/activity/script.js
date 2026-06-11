5952bf9206b6d66b79d050c10d42d7628a696b08
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-11 14:45:55 +0530

    feat(DOM): Add spacing in button click event for improved readability

--- Day 22/activity/script.js
+++ Day 22/activity/script.js
@@ -4,8 +4,10 @@ let btn = document.querySelector("button");
 btn.addEventListener("click", function(){
     let h3 = document.querySelector("h3");
     let randomColor = getRandomColor();
+
     h3.innerText = randomColor;
     console.log("Color Updated");
+    
     let div = document.querySelector("div");
     div.style.backgroundColor = randomColor;
   
