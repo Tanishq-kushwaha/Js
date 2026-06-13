fd44493078baf1bb825f1f77bd31c505c6d149fb
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-13 19:27:40 +0530

    feat(app.js): Implement mouseout and keypress event listeners with console logging

--- Day 22/assignmentQs/app.js
+++ Day 22/assignmentQs/app.js
@@ -1,10 +1,35 @@
 // Qs1. Try out the following events in Event Listener on your own :
 
 // - mouseout
+let div = document.querySelector(".container");
+
+
+    div.style.backgroundColor = "red";
+    div.style.width = "100px";
+    div.style.height = "100px";
+
+
+div.addEventListener("mouseout", function(){
+    this.style.backgroundColor = "blue";
+    this.style.width = "100px";
+    this.style.height = "100px";
+});
 
 // - keypress
 
+document.addEventListener("keypress", function(){
+  console.log("Key pressed");
+});
+
+
+document.addEventListener("keydown", function(){
+  console.log("Key down");
+});
+
 // - Scroll
+
+
+
 
 // - load
 
