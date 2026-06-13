0a53357c6d1460c452f1b42547f24aeafcfee498
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-13 19:54:16 +0530

    feat: Add input element for username and update heading dynamically

--- Day 22/assignmentQs/app.js
+++ Day 22/assignmentQs/app.js
@@ -52,3 +52,22 @@ document.body.appendChild(btn);
 btn.addEventListener("click", function(){
   this.style.backgroundColor = this.innerText;
 });
+
+
+// Qs3. Create an input element on the page with a placeholder ”enter your name” and an
+
+// H2 heading on the page inside HTML.
+
+// The purpose of this input element is to enter a user’s name so it should only input
+// letters from a-z, A-Z and space (all other characters should not be detected).
+// Whenever the user inputs their name, their input should be dynamically visible inside
+// the heading.
+// [Please note that no other character apart from the allowed characters should be
+// visible in the heading
+
+let heading = document.querySelector("h2");
+let inputBox = document.querySelector("input");
+
+ inputBox.addEventListener("input", function(){
+   heading.innerText = this.value;
+ });
