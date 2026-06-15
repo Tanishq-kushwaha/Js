f9cf7530ffb60dae89b8224e5752b5b79be2484d
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-15 19:40:26 +0530

    feat(DOM) :  add event listener in button

---
+++ Day 23 mini project/todo with Dom/app.js
@@ -0,0 +1,6 @@
+customElements button = document.querySelector("button");
+const inputBox = document.querySelector("input");
+
+button.addEventListener("click", function(){
+  let task = inputBox.value;
+});
