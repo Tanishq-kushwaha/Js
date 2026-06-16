e223f017bb7140113dfe59459f0bb97e2e8eddb5
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-16 19:48:24 +0530

    fix(app.js): Improve formatting and readability of event listener functions

--- Day 23 mini project/todo with Dom/app.js
+++ Day 23 mini project/todo with Dom/app.js
@@ -2,25 +2,25 @@ const button = document.querySelector("button");
 const inputBox = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-button.addEventListener("click", function(){
-  let task = inputBox.value;
-  let item = document.createElement("li");
-  item.innerText = task;
+button.addEventListener("click", function () {
+    let task = inputBox.value;
+    let item = document.createElement("li");
+    item.innerText = task;
 
-  let dltBtn = document.createElement("button");
-  dltBtn.innerText = "delete"; 
-  dltBtn.classList.add("delete");
-  item.appendChild(dltBtn);
+    let dltBtn = document.createElement("button");
+    dltBtn.innerText = "delete";
+    dltBtn.classList.add("delete");
+    item.appendChild(dltBtn);
 
-  ul.appendChild(item);
-  inputBox.value = "";
- 
+    ul.appendChild(item);
+    inputBox.value = "";
+
 });
 
 
- ul.addEventListener("click", function(event) { 
+ul.addEventListener("click", function (event) {
     if (event.target.nodeName === "BUTTON") {
-    let listItem = event.target.parentElement;
-    listItem.remove();
-}
- });
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+});
