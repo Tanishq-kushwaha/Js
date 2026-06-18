423a526eb33092d2723f055e5d3f38ec3f312974
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-18 20:30:37 +0530

    feat: Add event listener to start game on keypress

--- Day 23 mini project/simon game/app.js
+++ Day 23 mini project/simon game/app.js
@@ -3,4 +3,11 @@ let userSeq = [];
 
 let started = false;
 let level = 0;
- 
+
+document.addEventListener("keypress", function(){
+ if(started == false){
+    console.log("game started");
+    started = true;
+    
+ }
+});
