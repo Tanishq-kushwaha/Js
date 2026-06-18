b308fd2085cb871246cfef8ae84f6f469086512b
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-18 20:53:01 +0530

    feat(app.js): Define button colors and update random button selection logic in levelUp function

--- Day 23 mini project/simon game/app.js
+++ Day 23 mini project/simon game/app.js
@@ -1,6 +1,8 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btns = ["yellow","red","purple","green"];
+
 let started = false;
 let level = 0;
 
@@ -25,6 +27,7 @@ function levelUp() {
     level++;
     h2.innerText = (`Level ${level}`);
 
-    //random btn choose
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
     btnFlash();
 }
