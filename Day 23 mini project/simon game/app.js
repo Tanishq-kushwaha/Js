46131260d45709043026022a667b2bb4428987e2
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-18 20:59:17 +0530

    style(app.js): Format button array and improve btnFlash timeout duration

--- Day 23 mini project/simon game/app.js
+++ Day 23 mini project/simon game/app.js
@@ -1,7 +1,7 @@
 let gameSeq = [];
 let userSeq = [];
 
-let btns = ["yellow","red","purple","green"];
+let btns = ["yellow", "red", "purple", "green"];
 
 let started = false;
 let level = 0;
@@ -16,18 +16,18 @@ document.addEventListener("keypress", function () {
     }
 });
 
-function btnFlash(btn){
- btn.classList.add("flash");
- setTimeout(function(){
-    btn.classList.remove("flash");
- }, 1000);
+function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 400);
 }
 
 function levelUp() {
     level++;
     h2.innerText = (`Level ${level}`);
 
-    let randIdx = Math.floor(Math.random()*3);
+    let randIdx = Math.floor(Math.random() * 3);
     let randColor = btns[randIdx];
     let randbtn = document.querySelector(`.${randColor}`);
     btnFlash(randbtn);
