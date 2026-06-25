96c4bcc146a3c59c38848052c50d50590d96939c
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-25 20:22:22 +0530

    feat(app.js): Add changeColor function to modify header color with delay

--- Day 24/app.js
+++ Day 24/app.js
@@ -32,3 +32,10 @@ three();
 
 let h1 = document.querySelector("h1");
 
+function changeColor(color, delay, nextColorChange){
+ setTimeout(()=>{
+    h1.style.color = color;
+    if(nextColorChange) nextColorChange();
+ }, delay);
+}
+
