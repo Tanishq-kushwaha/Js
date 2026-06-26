4bdfd29c329275b2dc6bb618423ce1b2a79814fc
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-26 14:22:33 +0530

    feat(app.js): Add random  internetSpeed function

--- Day 24/app.js
+++ Day 24/app.js
@@ -72,6 +72,14 @@
 // }
 // flow();
 
+function saveToDb(data, success, failure){
+    let internetSpeed = Math.floor(Math.random()*10)+1;
+    if(internetSpeed > 4){
+        success();
+    }else{
+        failure();
+    }
+}
 
 
 
