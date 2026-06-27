21c5dfa18f0297aca1934fe9247eafd45b694b02
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-27 15:47:28 +0530

    feat(app.js): Refactor saveToDb function to return a Promise with success and failure messages

--- Day 24/app.js
+++ Day 24/app.js
@@ -98,6 +98,10 @@
 function saveToDb(data) {
   return new Promise((resolve, reject)=>{
     let internetSpeed = Math.floor(Math.random()*10)+1;
-    
+    if(internetSpeed > 4){
+        resolve("Success : data saved");
+    }else{
+        reject("failure : week internet speed");
+    }
   });
 }
