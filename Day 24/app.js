519dcd744958befa0cd88ddff8d7c146c6e7ce73
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-27 19:37:21 +0530

    feat(app.js): Enhance changeColor function with sequential color changes and logging

--- Day 24/app.js
+++ Day 24/app.js
@@ -37,11 +37,34 @@ function changeColor(color, delay) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             h1.style.color = color;
-            if (nextColorChange) nextColorChange();
+            resolve();
         }, delay);
     });
 }
 
+changeColor("red", 1000)
+    .then(() => {
+        console.log("red color was completed");
+        return changeColor("orange", 1000);
+    })
+    .then(() => {
+        console.log("orange color was completed");
+        return changeColor("green", 1000);
+    })
+    .then(() => {
+        console.log("green color was completed");
+        return changeColor("pink", 1000);
+    })
+    .then(() => {
+        console.log("Pink color was completed");
+        return changeColor("blue", 1000);
+    })
+    .then(() => {
+        console.log("blue color was completed");
+    })
+    .catch(() => {
+        console.log("syntax error");
+    });
 // function repeat() {
 //     changeColor("red", 1000, () => {
 //         changeColor("orange", 1000, () => {
