1d8aa08acd2b61959aeb0a54edb6beb3902f6ea9
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-27 15:40:35 +0530

    feat(app.js): Refactor saveToDb function to return a Promise

--- Day 24/app.js
+++ Day 24/app.js
@@ -96,10 +96,8 @@
 
 
 function saveToDb(data) {
-    let internetSpeed = Math.floor(Math.random() * 10) + 1;
-    if (internetSpeed > 4) {
-        success();
-    } else {
-        failure();
-    }
+  return new Promise((resolve, reject)=>{
+    let internetSpeed = Math.floor(Math.random()*10)+1;
+    
+  });
 }
