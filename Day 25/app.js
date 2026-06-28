91b09981073371b117bb67a76837ed4b60a2da76
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-28 17:54:51 +0530

    fix(app.js): Comment out greet function and related code for future reference

--- Day 25/app.js
+++ Day 25/app.js
@@ -1,19 +1,28 @@
-async function greet() {
-    // throw "error";
-    return "hello";
-}
+// async function greet() {
+//     // throw "error";
+//     return "hello";
+// }
 
 
-greet().then((result) => {
-    console.log("fulfilled");
-    console.log("Result was:", result);
-})
-    .catch(() => {
-        console.log("some error");
+// greet().then((result) => {
+//     console.log("fulfilled");
+//     console.log("Result was:", result);
+// })
+//     .catch(() => {
+//         console.log("some error");
+//     });
+
+
+// let demo = async () => {
+//     return 5;
+// }
+
+function getNum() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+           let num = Math.floor(Math.random() * 10) + 1;
+            console.log(num);
+            resolve();
+        }, 1000);
     });
-
-
-let demo = async () => {
-    return 5;
 }
-
