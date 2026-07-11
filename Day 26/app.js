e41f9e1a52521031d18e6486f4a2fbb9f3af18e0
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-07-11 20:25:03 +0530

    feat(app.js): Add getFact function to fetch and log cat facts from API

---
+++ Day 26/app.js
@@ -0,0 +1,10 @@
+let url = "https://catfact.ninja/fact";
+
+async function  getFact() {
+    try{
+        let res = await axios.get(url);
+        console.log(res);
+    }catch(e){
+        console.log("ERROR - ", e);
+    }
+}
