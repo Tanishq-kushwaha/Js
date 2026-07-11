e2090339d7efcd85a5de1afb4769434eee69c2b3
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-07-11 21:02:54 +0530

    feat(app.js): Update button click event to fetch and display cat facts

--- Day 26/app.js
+++ Day 26/app.js
@@ -1,12 +1,17 @@
 let btn = document.querySelector("button");
+let para = document.querySelector("p");
 
+
+btn.addEventListener("click", ()=>{
+  let fact =  getFact();
+});
 
 let url = "https://catfact.ninja/fact";
 
 async function  getFact() {
     try{
         let res = await axios.get(url);
-        console.log(res);
+        console.log(res.data.fact);
     }catch(e){
         console.log("ERROR - ", e);
     }
