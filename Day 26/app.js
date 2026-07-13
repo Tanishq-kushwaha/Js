73c2f02e333efe63fdcbb8993f35bdbd916445ab
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-07-13 14:51:23 +0530

    fix(app.js, index.html): Refactor event listener syntax and improve error handling for fetching facts and images

--- Day 26/app.js
+++ Day 26/app.js
@@ -1,6 +1,6 @@
 let btn = document.querySelector("#cat");
 
-btn.addEventListener("click", async()=>{
+btn.addEventListener("click", async () => {
     let fact = await getFact();
     let para = document.querySelector("#result");
     para.innerText = fact;
@@ -8,11 +8,11 @@ btn.addEventListener("click", async()=>{
 
 let url = "https://catfact.ninja/fact";
 
-async function  getFact() {
-    try{
+async function getFact() {
+    try {
         let res = await axios.get(url);
         return res.data.fact;
-    }catch(e){
+    } catch (e) {
         console.log("ERROR - ", e);
         return "no fact found";
     }
@@ -24,20 +24,32 @@ let btn2 = document.querySelector("#dogImg");
 
 let url2 = "https://dog.ceo/api/breeds/image/random";
 
-btn2.addEventListener("click", async()=>{
-let link = await getImg();
-let img = document.querySelector("#imgResult");
-img.setAttribute("src", link);
-// img.src = link;
+btn2.addEventListener("click", async () => {
+    let link = await getImg();
+    let img = document.querySelector("#imgResult");
+    img.setAttribute("src", link);
+    // img.src = link;
 });
 
 
 async function getImg() {
-    try{
-        let res = await axios(url2);
+    try {
+        let res = await axios.get(url2);
         return res.data.message;
-    }catch(e){
+    } catch (e) {
         console.log("ERROR - ", e);
         return "no Image found";
+    }
+}
+
+
+const joke = "https://icanhazdadjoke.com/";
+async function getJoke() {
+    try {
+        const config = { headers: { Accept: "application/json" } };
+        let res = await axios.get(joke, config);
+        console.log(res.data.joke);
+    } catch (err) {
+        console.log(err);
     }
 }
